68873bd8f9ce6e54837b20c704881a1af602eb36
tizou-cloud <chromgool@gmail.com>
2025-04-12 17:28:16 +0100

    Ajout du token BattleMetrics au backend

--- api/index.js
+++ api/index.js
@@ -1,26 +1,36 @@
 export default async function handler(req, res) {
-  const search = req.query.search;
-  const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbiI6ImU1YTA0MjIzM2U4ZTI4YTgiLCJpYXQiOjE3NDQwNDYzNjAsIm5iZiI6MTc0NDA0NjM2MCwiaXNzIjoiaHR0cHM6Ly93d3cuYmF0dGxlbWV0cmljcy5jb20iLCJzdWIiOiJ1cm46dXNlcjoxMDE1NDk1In0.UZHq5KbHgXycv2nN2ogSvuUXTwHEU0lBVVNrUlCmkVU";
+  const { search, steam, id } = req.query;
 
-  if (!search) {
-    return res.status(400).json({ error: "Paramètre ?search= requis" });
+  if (!search && !id) {
+    return res.status(400).json({ error: "Paramètre ?search= ou ?id= requis" });
   }
 
-  try {
-    const response = await fetch(`https://api.battlemetrics.com/players?filter[search]=${encodeURIComponent(search)}`, {
-      headers: {
-        Authorization: `Bearer ${token}`
-      }
-    });
+  const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbiI6ImU1YTA0MjIzM2U4ZTI4YTgiLCJpYXQiOjE3NDQwNDYzNjAsIm5iZiI6MTc0NDA0NjM2MCwiaXNzIjoiaHR0cHM6Ly93d3cuYmF0dGxlbWV0cmljcy5jb20iLCJzdWIiOiJ1cm46dXNlcjoxMDE1NDk1In0.UZHq5KbHgXycv2nN2ogSvuUXTwHEU0lBVVNrUlCmkVU";
 
-    if (!response.ok) {
-      const errorData = await response.json();
-      return res.status(500).json({ error: "Erreur BattleMetrics", details: errorData });
+  const headers = {
+    Authorization: `Bearer ${token}`,
+    "Content-Type": "application/json"
+  };
+
+  try {
+    let url = "https://api.battlemetrics.com/players";
+
+    if (id) {
+      url += `/${id}`;
+    } else if (steam) {
+      url += `?filter[steamID]=${search}`;
+    } else {
+      url += `?filter[search]=${search}`;
     }
 
+    const response = await fetch(url, { headers });
     const data = await response.json();
-    res.status(200).json(data);
 
+    if (response.ok) {
+      res.status(200).json(data);
+    } else {
+      res.status(500).json({ error: "Erreur BattleMetrics", details: data });
+    }
   } catch (err) {
     res.status(500).json({ error: "Erreur serveur", details: err.message });
   }
